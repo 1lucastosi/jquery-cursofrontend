69cb2f6f4473f34d14aa2e6ae958e1c4db935991
Lucas Gabriel <lucastosi2610@gmail.com>
2021-12-19 23:59:09 -0300

    Adicionando arquivos para o modulo 2

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+// instancia jQuery e evita conflitos
+
+jQuery(function($){
+
+    // Seletores Simples
+
+    $('h4') //tag
+    $('.featured-item') //class
+    $('#featured') //id
+
+    // Seletores Compostos
+
+    $('h4,h6')
+    $('div h4')
+
+
+})
